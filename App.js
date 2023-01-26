ca0d2ffec0a0b1e27d6ede959d52f028c5f4d323
Lukyord <ttansiri524@gmail.com>
2023-01-26 09:41:43 +0100

    implement navigation and app structure

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-purple-300">
-        Open up App.js to start working on your app!
-      </Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ StackNavigator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import ChatScreen from "./screens/ChatScreen/ChatScreen";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+  const user = true;
+
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+}

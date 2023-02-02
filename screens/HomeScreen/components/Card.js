016b56cc4c08fe3af7332a341f79f89f0dde5eaa
Lukyord <ttansiri524@gmail.com>
2023-02-02 09:06:28 +0100

    styling card

--- screens/HomeScreen/components/Card.js
+++ screens/HomeScreen/components/Card.js
@@ -11,14 +11,52 @@ export default function Card() {
           backgroundColor: "transparent",
         }}
         cards={DUMMY_DATA}
+        stackSize={5}
+        cardIndex={0}
+        animateCardOpacity
+        verticalSwipe={false}
+        onSwipedLeft={() => console.log("Swipe Pass")}
+        onSwipedRight={() => console.log("Swipe Match")}
+        backgroundColor={"#4FD0E9"}
+        overlayLabels={{
+          left: {
+            title: "NOPE",
+            style: {
+              label: {
+                textAlign: "right",
+                color: "red",
+              },
+            },
+          },
+          right: {
+            title: "MATCH",
+            style: {
+              label: {
+                color: "#4DED30",
+              },
+            },
+          },
+        }}
         renderCard={(card) => (
-          <View key={card.id} className="relative bg-white h-3/4 rounded-xl">
+          <View key={card.id} className="relative bg-white h-3/4 rounded-3xl">
             <Image
-              className="h-full w-full rounded-xl"
+              className="h-full w-full rounded-t-xl"
               source={{
                 uri: card.photoURL,
               }}
             />
+            <View
+              className="bg-white w-full h-20 flex flex-row justify-between 
+            items-between px-6 py-2 rounded-b-md shadow-2xl"
+            >
+              <View>
+                <Text className="text-xl font-bold">
+                  {card.firstName} {card.lastName}
+                </Text>
+                <Text>{card.occupation}</Text>
+              </View>
+              <Text className="text-2xl font-bold">{card.age}</Text>
+            </View>
           </View>
         )}
       />

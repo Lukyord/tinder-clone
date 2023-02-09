5a25be80b71487892e36dfdcf6a9413660af54f8
Lukyord <ttansiri524@gmail.com>
2023-02-09 09:57:22 +0100

    add .env

--- firebase.js
+++ firebase.js
@@ -5,15 +5,24 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDvCaX_uZIaEWlphyZLKXCuKIVbTE6RFf8",
+//   authDomain: "tinder-clone-584e7.firebaseapp.com",
+//   projectId: "tinder-clone-584e7",
+//   storageBucket: "tinder-clone-584e7.appspot.com",
+//   messagingSenderId: "587002959558",
+//   appId: "1:587002959558:web:e5846ab881e7e4f12da9c7",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDvCaX_uZIaEWlphyZLKXCuKIVbTE6RFf8",
-  authDomain: "tinder-clone-584e7.firebaseapp.com",
-  projectId: "tinder-clone-584e7",
-  storageBucket: "tinder-clone-584e7.appspot.com",
-  messagingSenderId: "587002959558",
-  appId: "1:587002959558:web:e5846ab881e7e4f12da9c7",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
